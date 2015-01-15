353b88ce34ebe095a689311adf5b73dde35054cb
Qiming Liu <qooroo@gmail.com>
2015-01-15 11:30:36 +0000

    sln setup

---
+++ src/KeepyUppy.Backplane/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using log4net.Config;
+using Topshelf;
+
+namespace KeepyUppy.Backplane
+{
+    class Program
+    {
+        static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Bootstrapper>(b =>
+                {
+                    b.ConstructUsing(_ => new Bootstrapper());
+                    b.WhenStarted(bs =>
+                    {
+                        XmlConfigurator.Configure();
+                        bs.Run();
+                    });
+                    b.WhenStopped(bs => bs.Dispose());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("KeepyUppy.Backplane");
+                x.SetDisplayName("KeepyUppy.Backplane");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/KeepyUppy.Backplane/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+
+namespace KeepyUppy.Backplane
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+#if DEBUG
+            app.UseErrorPage();
+#endif
+            app.UseWelcomePage("/");
+        }
+    }
+}
